55dd83d09eeafeea4afcfde2ddfd7042db9490dd
Tianrui Tu <112426336+TTU0906@users.noreply.github.com>
2023-07-26 23:50:28 -0400

    add eval

---
+++ eval.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from utils import *
+
+
+def evaluate(encoder, decoder, searcher, voc, sentence, device, max_length=10):
+    indexes_batch = [indexesFromSentence(voc, sentence)]
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
+    input_batch = input_batch.to(device)
+    lengths = lengths.to(device)
+    tokens, scores = searcher(input_batch, lengths, max_length)
+    decoded_words = [voc.index2word[token.item()] for token in tokens]
+    return decoded_words
+
+
+def evaluateInput(encoder, decoder, searcher, voc, device):
+    input_sentence = ''
+    while(1):
+        try:
+            input_sentence = input('> ')
+            if input_sentence == 'q' or input_sentence == 'quit': break
+            input_sentence = normalizeString(input_sentence)
+            output_words = evaluate(encoder, decoder, searcher, voc, input_sentence, device)
+            words = []
+            for word in output_words:
+                if word == 'EOS':
+                    break
+                elif word != 'PAD':
+                    words.append(word)
+            print('Bot:', ' '.join(words))
+
+        except KeyError:
+            print("Error: Encountered unknown word.")
